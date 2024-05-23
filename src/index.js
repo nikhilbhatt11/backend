4355117537ba536821fd6f6ea1fa9c32cfbb894f
nikhilbhatt11 <nikubhatt111@gmail.com>
2024-05-23 13:20:32 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGOBD_URI} / ${DB_NAME}`);
+//     app.on("error", () => {
+//       console.log("ERRR: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`APP is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("ERROR: ", error);
+//     throw error;
+//   }
+// })();
